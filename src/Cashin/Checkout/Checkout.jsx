0ff9bca49b2ecd6bb5bb567f8e2370ddbe0bf9a1
sajib689 <jibonraja11@gmail.com>
2023-09-08 11:12:04 +0600

    checkout page finally completed

--- src/Cashin/Checkout/Checkout.jsx
+++ src/Cashin/Checkout/Checkout.jsx
@@ -23,6 +23,30 @@ const Checkout = () => {
          
         </div>
       </div>
+      <div className="w-1/2 mx-auto mt-20">
+        <div className="flex flex-col">
+            <label htmlFor="">First Name</label>
+        <input type="text" placeholder="Enter you first name" className="input input-bordered w-full max-w-lg" />
+        <label htmlFor="">Last Name</label>
+        <input type="text" placeholder="Enter you last name" className="input input-bordered w-full max-w-lg" />
+        </div>
+        <div className="flex flex-col">
+        <label htmlFor="">Phone</label>
+        <input type="text" placeholder="Enter your phone" className="input input-bordered w-full max-w-lg" />
+        <label htmlFor="">Email</label>
+        <input type="text" placeholder="Enter your email" className="input input-bordered w-full max-w-lg" />
+        </div>
+        <div className="flex flex-col">
+        <label htmlFor="">Message</label>
+        <textarea
+        className="max-w-lg w-full p-2 border border-gray-300 rounded focus:outline-none focus:border-indigo-500"
+        id="message"
+        name="message"
+        rows="4"
+        placeholder="Type your message here..."
+      />            </div>
+        <input type="submit"  className="mt-7 text-white bg-orange-600 input input-bordered w-full max-w-lg" value='Order Now'/>
+      </div>
     </div>
   );
 };
